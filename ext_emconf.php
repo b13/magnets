731a376c4b5dc1a5c7ef8651e2c36a35454563b3
Daniel Goerz <daniel.goerz@b13.com>
2020-03-18 16:32:30 +0100

    [TASK] Make extension installable in TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'B13',
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.9.99'
+            'typo3' => '8.7.0-10.9.99'
         ],
         'conflicts' => [],
         'suggests' => [],
